4006303ad1031b7ed59b774f8b40c84b9fd3c1f5
Ondřej Seidl <seidon@seznam.cz>
2024-08-17 11:21:14 +0200

    Přidání indexů k atributům pro budoucí relace

--- ERP/SQL_ERP_Tables.sql
+++ ERP/SQL_ERP_Tables.sql
@@ -1,7 +1,6 @@
 USE Finance;
 
-CREATE TABLE IF NOT EXISTS Accounts
-(
+CREATE TABLE IF NOT EXISTS Accounts (
     Account_Id   INT AUTO_INCREMENT,
     Account_Name VARCHAR(255)                                                      NOT NULL,
     Account_Type ENUM ('Aktivum', 'Pasivum', 'Vlastní kapitál', 'Příjem', 'Výdej') NULL,
@@ -13,8 +12,7 @@ CREATE TABLE IF NOT EXISTS Accounts
 );
 
 
-CREATE TABLE IF NOT EXISTS Account_Book
-(
+CREATE TABLE IF NOT EXISTS Account_Book (
     Book_Id         INT AUTO_INCREMENT,
     Account_Id      INT           NOT NULL,
     Write_Date      DATE          NOT NULL,
@@ -26,8 +24,7 @@ CREATE TABLE IF NOT EXISTS Account_Book
 )
     COMMENT 'Účetní kniha obsahující veškeré záznamy';
 
-CREATE TABLE IF NOT EXISTS Invoices
-(
+CREATE TABLE IF NOT EXISTS Invoices (
     Invoice_Id   INT AUTO_INCREMENT,
     Invoice_Code NCHAR(100)                           NOT NULL,
     Customer_Id  INT                                  NOT NULL,
@@ -39,68 +36,71 @@ CREATE TABLE IF NOT EXISTS Invoices
         PRIMARY KEY ( Invoice_Id )
 );
 
-create table IF NOT EXISTS INVOICES_ITEMS
-(
-    invoice_item_id int auto_increment,
-    invoice_id      int           not null,
-    quantity        int           not null,
-    unit_price      DOUBLE(20, 2) not null,
-    total_price     DOUBLE(20, 2) AS (Quantity * Unit_Price),
-    constraint INVOICES_ITEMS_pk
-        primary key (invoice_item_id)
+CREATE TABLE IF NOT EXISTS Invoices_Items (
+    Invoice_Item_Id INT AUTO_INCREMENT,
+    Invoice_Id      INT           NOT NULL,
+    Quantity        INT           NOT NULL,
+    Unit_Price      DOUBLE(20, 2) NOT NULL,
+    Total_Price     DOUBLE(20, 2) AS (Quantity * Unit_Price),
+    CONSTRAINT Invoices_Items_Pk
+        PRIMARY KEY ( Invoice_Item_Id )
 );
 
-create table IF NOT EXISTS invoice_tax
-(
-    invoice_tax_id int auto_increment,
-    invoice_id     int           not null,
-    tax_id         int           not null,
-    tax_amount     DOUBLE(10, 2) not null,
-    constraint invoice_tax_pk
-        primary key (invoice_tax_id)
+CREATE TABLE IF NOT EXISTS Invoice_Tax (
+    Invoice_Tax_Id INT AUTO_INCREMENT,
+    Invoice_Id     INT           NOT NULL,
+    Tax_Id         INT           NOT NULL,
+    Tax_Amount     DOUBLE(10, 2) NOT NULL,
+    CONSTRAINT Invoice_Tax_Pk
+        PRIMARY KEY ( Invoice_Tax_Id )
 );
 
-create table  IF NOT EXISTS taxes
-(
-    tax_id   int auto_increment,
-    tax_rate DECIMAL(3, 2)                                 not null,
-    tax_type ENUM ('Daň z příjmu', 'Daň z prodeje', 'DPH') not null,
-    constraint taxes_pk
-        primary key (tax_id)
+CREATE TABLE IF NOT EXISTS Taxes (
+    Tax_Id   INT AUTO_INCREMENT,
+    Tax_Rate DECIMAL(3, 2)                                 NOT NULL,
+    Tax_Type ENUM ('Daň z příjmu', 'Daň z prodeje', 'DPH') NOT NULL,
+    CONSTRAINT Taxes_Pk
+        PRIMARY KEY ( Tax_Id )
 );
 
 
-create table IF NOT EXISTS Payments
-(
-    payment_id      int auto_increment,
-    invoice_id      int                                                           not null,
-    payment_date    DATE                                                          not null,
-    Amount          DOUBLE(20, 2)                                                 not null,
-    Payment_methond ENUM ('Kreditní karta', 'Hotovost', 'Bankovní převod', 'Šek') not null,
-    constraint Payments_pk
-        primary key (payment_id)
+CREATE TABLE IF NOT EXISTS Payments (
+    Payment_Id      INT AUTO_INCREMENT,
+    Invoice_Id      INT                                                           NOT NULL,
+    Payment_Date    DATE                                                          NOT NULL,
+    Amount          DOUBLE(20, 2)                                                 NOT NULL,
+    Payment_Methond ENUM ('Kreditní karta', 'Hotovost', 'Bankovní převod', 'Šek') NOT NULL,
+    CONSTRAINT Payments_Pk
+        PRIMARY KEY ( Payment_Id )
 );
 
-create table IF NOT EXISTS Customers_inv
-(
-    customer_id     int auto_increment,
-    Customer_name   NCHAR(255)   not null,
-    billing_address TEXT         null,
-    contact         varchar(255) not null,
-    constraint Customers_pk
-        primary key (customer_id)
+CREATE TABLE IF NOT EXISTS Customers_Inv (
+    Customer_Id     INT AUTO_INCREMENT,
+    Customer_Name   NCHAR(255)   NOT NULL,
+    Billing_Address TEXT         NULL,
+    Contact         VARCHAR(255) NOT NULL,
+    CONSTRAINT Customers_Pk
+        PRIMARY KEY ( Customer_Id )
 );
 
-create table IF NOT EXISTS Vendors
-(
-    vendor_id int auto_increment,
-    vendor_name   NCHAR(255)   not null,
-    billing_address TEXT         null,
-    contact         varchar(255) not null,
-    constraint Vendors_pk
-        primary key (vendor_id)
+CREATE TABLE IF NOT EXISTS Vendors (
+    Vendor_Id       INT AUTO_INCREMENT,
+    Vendor_Name     NCHAR(255)   NOT NULL,
+    Billing_Address TEXT         NULL,
+    Contact         VARCHAR(255) NOT NULL,
+    CONSTRAINT Vendors_Pk
+        PRIMARY KEY ( Vendor_Id )
 );
 
 
+create index account_book_account_id_index
+    on account_book (account_id);
 
+create index invoices_customer_id_index
+    on invoices (customer_id);
 
+create index invoices_items_invoice_id_index
+    on invoices_items (invoice_id);
+
+create index payments_invoice_id_index
+    on payments (invoice_id);
